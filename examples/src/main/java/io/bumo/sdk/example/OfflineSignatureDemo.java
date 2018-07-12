5aee9f70b6e5416a1dd561808eb5a1e479aad00f
riven <frm430342210@163.com>
2018-07-12 16:42:45 +0800

    Update the OfflineSignatureDemo

--- examples/src/main/java/io/bumo/sdk/example/OfflineSignatureDemo.java
+++ examples/src/main/java/io/bumo/sdk/example/OfflineSignatureDemo.java
@@ -1,9 +1,12 @@
 package io.bumo.sdk.example;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import io.bumo.SDK;
 import io.bumo.common.ToBaseUnit;
+import io.bumo.encryption.exception.EncException;
+import io.bumo.encryption.key.PrivateKey;
 import io.bumo.encryption.utils.hash.HashUtil;
 import io.bumo.encryption.utils.hex.HexFormat;
 import io.bumo.model.request.*;
@@ -19,34 +22,39 @@ import org.junit.Test;
 public class OfflineSignatureDemo {
     SDK sdk = SDK.getInstance("http://seed1.bumotest.io:26002");
     /**
-     * 1. 有网环境下，生成交易Blob
-     * 操作如下： (1). 获取交易发送账户的nonce
-     *          (2). 构建sendBU操作
-     *          (3). 构建交易
-     *          (4). 获取交易BLob串
+     * @Description 1. 有网环境下，生成交易Blob
+     * @Author riven
+     * @Method Online_BuildTransactionBlob
+     * @Params []
+     * @Return void
+     * @Date 2018/7/12 16:10
      */
     @Test
     public void Online_BuildTransactionBlob() {
-        String senderAddresss = "buQnnUEBREw2hB6pWHGPzwanX7d28xk6KVcp";                     // 发送方私钥
-        String destAddress = "buQBjJD1BSJ7nzAbzdTenAhpFjmxRVEEtmxH";                          // 接收方账户地址
-        Long amount = ToBaseUnit.BU2MO("10.9");                                      // 发送转出10.9BU给接收方（目标账户）
-        Long gasPrice = 1000L;                                                                // 固定写 1000L ，单位是MO
-        Long feeLimit = ToBaseUnit.BU2MO("0.01");                                    //设置最多费用 0.01BU ，固定填写
-        Long nonce = 32L;                                                                     // 参考getAccountNonce()获取账户Nonce + 1;
+        String senderAddresss = "buQnnUEBREw2hB6pWHGPzwanX7d28xk6KVcp"; // 发送方账户地址
+        String destAddress = "buQBjJD1BSJ7nzAbzdTenAhpFjmxRVEEtmxH"; // 接收方账户地址
+        Long amount = ToBaseUnit.BU2MO("10.9"); // 发送转出10.9BU给接收方（目标账户）
+        Long gasPrice = 1000L; // 固定写 1000L ，单位是MO
+        Long feeLimit = ToBaseUnit.BU2MO("0.01"); //设置最多费用 0.01BU ，固定填写
+        Long nonce = 0L; // 发送方账户Nonce
 
         // 构建交易Blob，返回transactionBlobResult（包含交易Blob和交易hash）
         String transactionBlobResult = buildTransactionBlob(senderAddresss, nonce, destAddress, amount, feeLimit, gasPrice);
         System.out.println(transactionBlobResult);
     }
-
+    
     /**
-     * 2. 无网环境下，解析交易Blob
-     * 操作如下： 解析出交易内容
+     * @Description 2. 无网环境下，解析交易Blob
+     * @Author riven
+     * @Method Offline_ParseBlob
+     * @Params []
+     * @Return void
+     * @Date 2018/7/12 16:11
      */
     @Test
     public void Offline_ParseBlob() {
-        // 从 1（有网环境） 中拷贝到的 transactionBlobResult
-        String transactionBlobResult = "{\"transaction_blob\":\"0A246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370102018C0843D20E8073A56080712246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370522C0A24627551426A4A443142534A376E7A41627A6454656E416870466A6D7852564545746D78481080A9E08704\",\"hash\":\"de655ab4136af2be9dc4ca345672728d762145722d0446d758bd420e1f4e0787\"}";
+        // 从 1(有网环境) 中拷贝到的 transactionBlobResult
+        String transactionBlobResult = "{\"transaction_blob\":\"0A246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370102118C0843D20E8073A56080712246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370522C0A24627551426A4A443142534A376E7A41627A6454656E416870466A6D7852564545746D78481080A9E08704\",\"hash\":\"d8fde921219ce265acb51e2cffbe7855e6423f795781e1810595159d9c104522\"}";
 
         // 解析交易Blob
         JSONObject transaction = parseBlob(transactionBlobResult);
@@ -56,13 +64,20 @@ public class OfflineSignatureDemo {
     }
 
     /**
-     * 3. 无网环境下，签名交易Blob
-     * 操作如下： 签名交易
+     * @Description 3. 无网环境下，签名交易Blob
+     * @Author riven
+     * @Method Offline_SignTransactionBlob
+     * @Params []
+     * @Return void
+     * @Date 2018/7/12 16:12
      */
     @Test
     public void Offline_SignTransactionBlob() {
         // 确认交易Blob无误后，开始进行签名
-        String transactionBlob = "0A246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370102018C0843D20E8073A56080712246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370522C0A24627551426A4A443142534A376E7A41627A6454656E416870466A6D7852564545746D78481080A9E08704";
+
+        // 交易Blob
+        String transactionBlob = "0A246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370102118C0843D20E8073A56080712246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370522C0A24627551426A4A443142534A376E7A41627A6454656E416870466A6D7852564545746D78481080A9E08704";
+        // 发送方私钥
         String senderPrivateKey = "privbyQCRp7DLqKtRFCqKQJr81TurTqG6UKXMMtGAmPG3abcM9XHjWvq";
 
         // 签名交易
@@ -73,14 +88,18 @@ public class OfflineSignatureDemo {
     }
 
     /**
-     * 4. 有网环境下，广播交易
-     * 操作如下： 广播交易
+     * @Description 4. 有网环境下，广播交易
+     * @Author riven
+     * @Method Online_SubmitTransaction
+     * @Params []
+     * @Return void
+     * @Date 2018/7/12 16:13
      */
     @Test
     public void Online_SubmitTransaction() {
         // 拿到 3（无网环境） 中的signature
-        String signature = "{\"signatures\":[{\"public_key\":\"b0011765082a9352e04678ef38d38046dc01306edef676547456c0c23e270aaed7ffe9e31477\",\"sign_data\":\"20FB5467E4283359B1226B3699993349C58FBBE2ECC8702B56534C355ED650184027EC931038BD49422EF2C4EDF6E4F406DA340D946BF6A79860C2DE19735804\"}]}";
-        String transactionBlob = "0A246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370102018C0843D20E8073A56080712246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370522C0A24627551426A4A443142534A376E7A41627A6454656E416870466A6D7852564545746D78481080A9E08704";
+        String signature = "{\"signatures\":[{\"public_key\":\"b0011765082a9352e04678ef38d38046dc01306edef676547456c0c23e270aaed7ffe9e31477\",\"sign_data\":\"D2B5E3045F2C1B7D363D4F58C1858C30ABBBB0F41E4B2E18AF680553CA9C3689078E215C097086E47A4393BCA715C7A5D2C180D8750F35C6798944F79CC5000A\"}]}";
+        String transactionBlob = "0A246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370102118C0843D20E8073A56080712246275516E6E5545425245773268423670574847507A77616E5837643238786B364B566370522C0A24627551426A4A443142534A376E7A41627A6454656E416870466A6D7852564545746D78481080A9E08704";
 
         // 提交交易，并返回交易hash
         String hash = submitTransaction(transactionBlob, signature);
@@ -89,6 +108,19 @@ public class OfflineSignatureDemo {
         }
     }
 
+    /**
+     * @Description 构建生成交易Blob
+     * @Author riven
+     * @Method buildTransactionBlob
+     * @Params senderAddresss: 发送方账户地址
+     *         nonce: 发送方账户Nonce
+     *         destAddress: 接收方账户地址
+     *         amount: 待发送给接收方的BU数量，单位是MO，1 BU = 10^8 MO
+     *         feeLimit: 交易费用，默认 0.01 BU
+     *         gasPrice: 交易打包费用, 默认 1000 MO
+     * @Return java.lang.String 交易Blob信息，包含交易Hash和交易Blob
+     * @Date 2018/7/12 16:10
+     */
     private String buildTransactionBlob(String senderAddresss, Long nonce, String destAddress, Long amount, Long feeLimit, Long gasPrice) {
         // 1. 获取交易发送账户的nonce
         AccountGetNonceRequest accountGetNonceRequest = new AccountGetNonceRequest();
@@ -127,7 +159,15 @@ public class OfflineSignatureDemo {
             return null;
         }
     }
-
+    
+    /**
+     * @Description 解析交易Blob
+     * @Author riven
+     * @Method parseBlob
+     * @Params transactionBlobResult: 交易Blob信息
+     * @Return com.alibaba.fastjson.JSONObject 交易Blob内容
+     * @Date 2018/7/12 16:19
+     */
     private JSONObject parseBlob(String transactionBlobResult) {
         // 验证交易Blob的正确性
         TransactionBuildBlobResult transactionBuildBlobResult = JSONObject.parseObject(transactionBlobResult, TransactionBuildBlobResult.class);
@@ -155,28 +195,52 @@ public class OfflineSignatureDemo {
         }
     }
 
+    /**
+     * @Description 签名交易
+     * @Author riven
+     * @Method signTransaction
+     * @Params transactionBlob: 交易Blob
+     *         senderPrivateKey: 发送方账户私钥
+     * @Return java.lang.String 签名结果，包括签名信息和公钥
+     * @Date 2018/7/12 16:20
+     */
     private String signTransaction(String transactionBlob, String senderPrivateKey) {
-        TransactionSignRequest transactionSignRequest = new TransactionSignRequest();
-        transactionSignRequest.setBlob(transactionBlob);
-        transactionSignRequest.addPrivateKey(senderPrivateKey);
+        try {
+            // 签名交易，并生成公钥
+            PrivateKey privateKey = new PrivateKey(senderPrivateKey);
+            byte[] signDataBytes = privateKey.sign(HexFormat.hexToByte(transactionBlob));
+            String signData = HexFormat.byteToHex(signDataBytes);
+            String publicKey = privateKey.getEncPublicKey();
 
-        // 签名交易
-        TransactionSignResponse transactionSignResponse = sdk.getTransactionService().sign(transactionSignRequest);
-        if (transactionSignResponse.getErrorCode() == 0) {
-            TransactionSignResult transactionSignResult = transactionSignResponse.getResult();
-            JSONObject signResultJson = (JSONObject) JSON.toJSON(transactionSignResult);
-            return signResultJson.toJSONString();
-        } else {
-            System.out.println("error: " + transactionSignResponse.getErrorDesc());
-            return null;
+            // 组装signature
+            JSONObject signatureJson = new JSONObject();
+            JSONArray signatures = new JSONArray();
+            JSONObject signature = new JSONObject();
+            signature.put("sign_data", signData);
+            signature.put("public_key", publicKey);
+            signatures.add(signature);
+            signatureJson.put("signatures", signatures);
+
+            return signatureJson.toJSONString();
+        } catch (EncException encException) {
+            encException.printStackTrace();
         }
+        return null;
     }
 
+    /**
+     * @Author riven
+     * @Method submitTransaction
+     * @Params transactionBlob: 交易Blob
+     *         signature: 签名私钥
+     * @Return java.lang.String
+     * @Date 2018/7/12 16:21
+     */
     private String submitTransaction(String transactionBlob, String signature) {
         TransactionSignResult transactionSignResult = JSONObject.parseObject(signature, TransactionSignResult.class);
         TransactionSubmitRequest transactionSubmitRequest = new TransactionSubmitRequest();
         transactionSubmitRequest.setTransactionBlob(transactionBlob);
-        transactionSignResult.setSignatures(transactionSignResult.getSignatures());
+        transactionSubmitRequest.setSignatures(transactionSignResult.getSignatures());
 
         // 提交交易
         TransactionSubmitResponse transactionSubmitResponse = sdk.getTransactionService().submit(transactionSubmitRequest);
